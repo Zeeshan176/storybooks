b5b7a2893188561bc97790d1ccb46b18cb199421
Md Zeeshan <Zeeshanmd956@gmail.com>
2021-10-25 12:19:17 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb+srv://zeeshanmd:7979@cluster0.31cvj.mongodb.net/storybooks-dev?retryWrites=true&w=majority',
+    googleClientID:'747436195142-ibtr9q3u0g079363ibo4m1c5j1suqtg5.apps.googleusercontent.com',
+    googleClientSecret:'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'
+}
